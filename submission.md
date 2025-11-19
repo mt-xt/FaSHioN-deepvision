ff528440ba6e7cc49fdaffb46eda00423f91dd4b
Matthew Abalos <muabalos@ucdavis.edu>
2025-11-18 17:18:32 -0800

    Fixed data in plot, displayed examples of misclassified data, finalized md file

--- submission.md
+++ submission.md
@@ -1,25 +1,44 @@
-# Resources used:
-https://stackoverflow.com/questions/37891954 keras-how-do-i-predict-after-i-trained-a-model
-https://stackoverflow.com/questions/44747343/keras-input-explanation-input-shape-units-batch-size-dim-etc
-https://keras.io/guides/sequential_model
+# Project 2: FASHION MNST IMAGE CLASSIFICATION
+## Note: There is User Input for number of epochs 
 
-# Print the number of trainable parameters in the model
-Number of trainable parameters: 394530
-
-# Evaluate training and validation accuracy at the end of each epoch, and plot them as line plots on the same set of axes.
+### Q1. Number of trainable parameters in the model
+Number of trainable parameters: **394530**
+### Q2. Trends between Training and Validation Accuracy At The End Of Each Epoch
 ![Training History](imgs/training_validation_accuracy_loss.png)
+### Q3. Evaluate accuracy on the test set
+Final Test Accuracy: **0.909500002861023**
 
-# Evaluate accuracy on the test set.
-313/313 ━━━━━━━━━━━━━━━━━━━━ 1s 1ms/step - accuracy: 0.9103 - loss: 0.3278
+Other Tests (Playing around with the layers):
+- Final Test Accuracy: 0.9041000008583069 @ 5 epochs using 2 additional convolution layers + 1 additional maxpooling layer
+- Final Test Accuracy: 0.9041000008583069 @ 20 epochs using 2 additional convolution layers + 1 additional maxpooling layer
 
-# Show an example from the test set for each class where the model misclassifies.
+### Q4.Examples of Misclassifcation (Test Set)
+![Misclassification](imgs/misclassified_examples.png)
+
+- 0   T-shirt/top
+- 1	Trouser
+- 2	Pullover
+- 3	Dress
+- 4	Coat
+- 5	Sandal
+- 6	Shirt
+- 7	Sneaker
+- 8	Bag
+- 9	Ankle boot
+
+### Observations
+In Q2 we plotted the trends between training accuracy vs validation accuracy, this provided us valuable information to what's happening within our CNN model. It seems that past Epoch 2 to 10, the training accuracy continues at a linear rate while validation accuracy begins to plataeu. This is roughly a 5% discrepancy which is insightful for us. This highlights a crucial issue that arises in our model of the data it's able to classify. As our model progresses, it's unable to improve classifying unshown information in our validation set, only improving on already learned information(overfitting). This issue is also related with Q4, where we projected examples of where our model fails to misclassify the clothing. An example is our model mistaken sandals to be as boots, this where I recognized an issue could be due to the lack of understanding small details. Sandals holds several unique characteristics such as flatter bottom, lack of covering on the top, and short in height. I think we could improve our model through data augmentation introducing different kinds of data from our existing one. Specifically padding, from my understand can help add in pixels to outter edges of an image to keep its shape(?).
 
 
+### Resources
+1. https://stackoverflow.com/questions/37891954/keras-how-do-i-predict-after-i-trained-a-model/
 
+2. https://stackoverflow.com/questions/44747343/keras-input-explanation-input-shape-units-batch-size-dim-etc
 
-# Comment on any other observations about the model performance
--first run through final epoch resulted in an accuracy around 0.977~ 
--Inconsistency between multiple runs, it tends to perform better when I'm not playing video games (LOL).
-    However, I read that you could use your GPU to run these computations instead of your CPU through "CUDA"
-    so I plan on implementing that. 
--I asked other classmates through the discord to capture a general range for the final test accuracy.
+3. https://keras.io/guides/sequential_model
+
+4. https://www.reddit.com/r/learnmachinelearning/comments/fjfr3y/can_someone_explain_train_and_validation_accuracy/
+
+5. https://medium.com/accredian/increase-the-performance-of-image-classification-models-b466e1ae3101
+
+6. https://aws.amazon.com/what-is/overfitting/
--- submission.py
+++ submission.py
@@ -1,16 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
-from sklearn.model_selection import train_test_split
+# import CUDA
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from tensorflow.keras import layers # condensed imports smfh
 from tensorflow.keras.optimizers import Adam
 from tensorflow import keras
-from tensorflow.keras.datasets import fashion_mnist
+
 
 BATCH_SIZE = 32 # Example batch size
-RANDOM_SEED = 42 # not necessary i think
+# RANDOM_SEED = 42  not necessary i think
 
 epoch_input = input("Enter number of epochs to train the model: ")
 EPOCHS = int(epoch_input)
@@ -38,6 +37,8 @@ train_x = train_x.reshape((48000, 28, 28, 1))
 validation_x = validation_x.reshape((12000, 28, 28, 1))
 x_test = x_test.reshape((10000, 28, 28, 1))
 
+# Begin training
+
 '''Layer Specifications:
     2D convolutional layer, 28 filters, 3x3 window size, ReLU activation
     2x2 max pooling
@@ -46,12 +47,15 @@ x_test = x_test.reshape((10000, 28, 28, 1))
     fully-connected layer, 10 nodes, softmax activation'''
 
 model = Sequential([
-    Conv2D(filters=28, kernel_size=3, activation='relu', input_shape=(28, 28, 1)), # CONVO LAYERS
-    MaxPooling2D((2, 2)),
-    Conv2D(filters=56, kernel_size=3, activation='relu'), # CONVO LAYERS, PATTERN DETECTION
-    Flatten(), # why do we need this?
-    Dense(units=56, activation='relu'),
-    Dense(units=10, activation='softmax')
+    layers.Conv2D(filters=28, kernel_size=3, activation='relu', input_shape=(28, 28, 1)), # CONVO lAYERS
+    # layers.Conv2D(filters=28, kernel_size=3, activation='relu', input_shape=(28, 28, 1)), # delete
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(filters=56, kernel_size=3, activation='relu'), # CONVO lAYERS, PATTERN DETECTION
+    # layers.Conv2D(filters=56, kernel_size=3, activation='relu'), # delete
+    # layers.MaxPooling2D((2, 2)), # delete
+    layers.Flatten(), 
+    layers.Dense(units=56, activation='relu'),
+    layers.Dense(units=10, activation='softmax')
 ])
 
 model.compile(
@@ -63,23 +67,6 @@ model.compile(
 data = model.fit(train_x, train_y, epochs=EPOCHS, batch_size=32,validation_data=(validation_x, validation_y))
 model.summary()
 # print(data.history.keys())
-# dict_keys(['accuracy', 'loss', 'val_accuracy', 'val_loss'])
-
-# Evaluate training and validation accuracy at the end of each epoch, and plot them as line plots on the same set of axes.
-plt.figure(figsize=(12, 7))
-plt.plot(data.history['loss'], label='Training Loss', marker='x')
-plt.plot(data.history['accuracy'], label='Validation Accuracy', marker='o')
-plt.xticks(range(0, len(data.history['accuracy'])))  # Every epoch
-plt.yticks(np.arange(0, 1.0, 0.025))  
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.title('Model Accuracy')
-plt.legend()
-plt.grid(True)
-
-plt.savefig('imgs/training_validation_accuracy_loss.png')
-plt.tight_layout()
-plt.show()
 
 # Print the number of trainable parameters in the model
 print(f"Number of trainable parameters: {model.count_params()}")
@@ -88,4 +75,67 @@ print(f"Number of trainable parameters: {model.count_params()}")
 test_loss, test_accuracy = model.evaluate(x_test, y_test)
 print(f"Final Test Accuracy: {test_accuracy}")
 
-# TODO Show an example from the test set for each class where the model misclassifies.
+# Show an example from the test set for each class where the model misclassifies.
+predictions = model.predict(x_test) 
+misclassified_indices = []
+for i in range(len(y_test)):
+    if np.argmax(predictions[i]) != y_test[i]:
+        misclassified_indices.append(i)
+
+# Note: Each training and test example is assigned to one of the following labels
+# 0	T-shirt/top
+# 1	Trouser
+# 2	Pullover
+# 3	Dress
+# 4	Coat
+# 5	Sandal
+# 6	Shirt
+# 7	Sneaker
+# 8	Bag
+# 9	Ankle boot
+
+# Clusters too much on the graph; removed for now
+# label = {0: "T-shirt/top", 
+#          1: "Trouser", 
+#          2: "Pullover", 
+#          3: "Dress", 
+#          4: "Coat", 
+#          5: "Sandal", 
+#          6: "Shirt", 
+#          7: "Sneaker", 
+#          8: "Bag", 
+#          9: "Ankle boot"}
+
+displayed_classes = set()
+plt.figure(figsize=(5, 5))
+count = 0
+for idx in misclassified_indices:
+    if count == 6:
+        break
+    true_label = y_test[idx]
+    if true_label not in displayed_classes:
+        plt.subplot(3, 4, len(displayed_classes) + 1)
+        plt.imshow(x_test[idx].reshape(28, 28), cmap='gray')
+        plt.title(f'Answer: {true_label} \n Pred: {np.argmax(predictions[idx])}') # Corrct Ans and prediction 
+        plt.axis('off')
+        displayed_classes.add(true_label)
+        count += 1
+plt.tight_layout()
+plt.savefig('imgs/misclassified_examples.png')
+plt.show()
+
+# dict_keys(['accuracy', 'loss', 'val_accuracy', 'val_loss'])
+# Evaluate training and validation accuracy at the end of each epoch, and plot them as line plots on the same set of axes.
+plt.figure(figsize=(9, 7))
+plt.plot(data.history['accuracy'], label='Training Accuracy', marker='o')
+plt.plot(data.history['val_accuracy'], label='Validation Accuracy', marker='x')
+plt.xticks(range(0, len(data.history['accuracy'])))  # Every epoch
+plt.yticks(np.arange(0.75, 1.0, 0.025))  
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Model Accuracy')
+plt.legend()
+plt.grid(True)
+plt.savefig('imgs/training_validation_accuracy_loss.png')
+plt.tight_layout()
+plt.show()
